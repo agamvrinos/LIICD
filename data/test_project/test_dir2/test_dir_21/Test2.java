45e012dc56e3934e34c254aaecd0f03845e22ef4
agamvrinos <agg_gam@hotmail.com>
2020-03-04 16:23:28 +0100

    Calculate md5 hash for code chunks

---
+++ data/test_project/test_dir2/test_dir_21/Test2.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+import java.io.*;
+
+class Test {
+    public static void main(String []args) {
+        System.out.println("Hello world");
+    }
+}
--- main/main.py
+++ main/main.py
@@ -2,20 +2,28 @@ from index.CloneIndex import CloneIndex
 from index.IndexEntry import IndexEntry
 from CodebaseReader import CodebaseReader
 from pathlib import Path
+import hashlib
+
+CHUNK_SIZE = 4
 
 codebase = CodebaseReader(Path.home() / 'PycharmProjects/CloneDetector/data/test_project')
 lines_per_files = codebase.get_lines_per_file()
 
-for key in lines_per_files.keys():
+clone_index = CloneIndex()
+
+for file in lines_per_files.keys():
     print('============================================================================================================')
-    print('[FILE]: ' + key)
-    print('[LINES]: ' + str(lines_per_files[key]))
+    print('[FILE]: ' + file)
+    for i in range(0, len(lines_per_files[file]) - CHUNK_SIZE + 1):
+        print("========================= BLOCK " + str(i) + " =========================")
+        block_str = ''
+        for j in range(i, i + CHUNK_SIZE):
+            block_str += lines_per_files[file][j] + "\n"
+        print(block_str)
+        block_str_hash = hashlib.md5(block_str.encode("utf-8")).hexdigest()
+        index_entry = IndexEntry(file, i, block_str_hash, i, i + CHUNK_SIZE)
+        clone_index.add_index_entry(index_entry)
+
 print("============================================================================================================")
 
-index_entry = IndexEntry("Test.java", 0, "43453", 0, 4)
-index_entry_2 = IndexEntry("Test.java", 1, "43455", 1, 5)
-
-clone_index = CloneIndex()
-clone_index.add_index_entry(index_entry)
-clone_index.add_index_entry(index_entry_2)
 clone_index.print_index()
