5396788d2141632d67ff2d7cd5906f7bfe22c31d
agamvrinos <agg_gam@hotmail.com>
2020-03-05 12:52:38 +0100

    Refactor code

---
+++ main/index/CloneDetector.py
@@ -0,0 +1,9 @@
+class CloneDetector:
+
+    def __init__(self, clone_index):
+        self.clone_index = clone_index
+
+    def detect_clones(self, index_entries):
+        # index_entries: index entries of the file to be checked against the existing
+        # TODO: Implement
+        return None
--- main/main.py
+++ main/main.py
@@ -1,3 +1,4 @@
+from index.CloneDetector import CloneDetector
 from index.CloneIndex import CloneIndex
 from index.IndexEntry import IndexEntry
 from CodebaseReader import CodebaseReader
@@ -6,24 +7,37 @@ import hashlib
 
 CHUNK_SIZE = 4
 
+
+def calculate_index_entries_for_file(file, lines):
+    print('============================================================================================================')
+    print('[FILE]: ' + file)
+    index_entries = []
+    for i in range(0, len(lines) - CHUNK_SIZE + 1):
+        print("========================= BLOCK " + str(i) + " =========================")
+        block_str = ''
+        for j in range(i, i + CHUNK_SIZE):
+            block_str += lines[j] + "\n"
+        print(block_str)
+        block_str_hash = hashlib.md5(block_str.encode("utf-8")).hexdigest()
+        index_entry = IndexEntry(file, i, block_str_hash, i, i + CHUNK_SIZE)
+        index_entries.append(index_entry)
+    return index_entries
+
+
 codebase = CodebaseReader(Path.home() / 'PycharmProjects/CloneDetector/data/test_project')
 lines_per_files = codebase.get_lines_per_file()
 
 clone_index = CloneIndex()
 
 for file in lines_per_files.keys():
-    print('============================================================================================================')
-    print('[FILE]: ' + file)
-    for i in range(0, len(lines_per_files[file]) - CHUNK_SIZE + 1):
-        print("========================= BLOCK " + str(i) + " =========================")
-        block_str = ''
-        for j in range(i, i + CHUNK_SIZE):
-            block_str += lines_per_files[file][j] + "\n"
-        print(block_str)
-        block_str_hash = hashlib.md5(block_str.encode("utf-8")).hexdigest()
-        index_entry = IndexEntry(file, i, block_str_hash, i, i + CHUNK_SIZE)
-        clone_index.add_index_entry(index_entry)
-
+    index_entries = calculate_index_entries_for_file(file, lines_per_files[file])
+    for entry in index_entries:
+        clone_index.add_index_entry(entry)
 print("============================================================================================================")
 
 clone_index.print_index()
+
+clone_detector = CloneDetector(clone_index)
+# clone_detector.detect_clones()
+
+
