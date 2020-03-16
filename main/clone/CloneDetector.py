e8fd39adbd89abcaccd565317490f5909d1b16d4
agamvrinos <agg_gam@hotmail.com>
2020-03-16 20:32:32 +0100

    Abstract filtering functionality

--- main/clone/CloneDetector.py
+++ main/clone/CloneDetector.py
@@ -1,20 +1,17 @@
 from typing import Dict, List
 from index.IndexEntry import IndexEntry
 from index.IndexEntriesGroup import IndexEntriesGroup
+from clone.Filter import Filter
 from clone.CloneGroup import CloneGroup
 from clone.ClonePart import ClonePart
 
 
 class CloneDetector:
-    NAME_COMPARATOR = 'NAME_COMPARATOR'
-    NAME_UNIT_COMPARATOR = 'NAME_UNIT_COMPARATOR'
 
     def __init__(self, clone_index):
-        self.filtered = []
+        self.filter = Filter()
         self.clone_index = clone_index
         self.origin_file_name = ''
-        self.l1 = None
-        self.l2 = None
 
     def detect_clones(self, index_entries):
         """
@@ -49,7 +46,7 @@ class CloneDetector:
                 if active_set.get__size() < 2 or active_set.subset_of(same_hash_block_groups[i - 1], j - i + 1):
                     break
 
-        return self.filtered
+        return self.filter.get__filtered()
 
     def create_groups(self, index_entries):
         groups_by_hash: Dict[str, IndexEntriesGroup] = {}
@@ -111,73 +108,4 @@ class CloneDetector:
 
         clone_group: CloneGroup = CloneGroup(parts, length, origin)
 
-        self.add_in_filter(clone_group)
-
-    def add_in_filter(self, current_clone_group: CloneGroup):
-        for earlier_clone_group in self.filtered:
-            if self.contains_in(current_clone_group, earlier_clone_group):
-                return
-            if self.contains_in(earlier_clone_group, current_clone_group):
-                self.filtered.remove(earlier_clone_group)
-
-        self.filtered.append(current_clone_group)
-
-    def contains_in(self, first: CloneGroup, second: CloneGroup):
-        if first.get__group_length() > second.get__group_length():
-            return False
-        first_parts: List[ClonePart] = first.get__parts()
-        second_parts: List[ClonePart] = second.get__parts()
-        self.l1 = first.get__group_length()
-        self.l2 = second.get__group_length()
-
-        return self.contains(first_parts, second_parts, self.NAME_UNIT_COMPARATOR) and \
-               self.contains(first_parts, second_parts, self.NAME_COMPARATOR)
-
-    def contains(self, container: List[ClonePart], list1: List[ClonePart], comparator):
-        container_index = 0
-        list_index = 0
-
-        while container_index < len(container) and list_index < len(list1):
-            container_part: ClonePart = container[container_index]
-            list_part: ClonePart = list1[list_index]
-
-            if comparator == self.NAME_COMPARATOR:
-                compare = self.compare_by_filename(container_part, list_part)
-            else:
-                compare = self.compare_by_filename_and_unit(container_part, list_part)
-
-            if compare == 0:
-                if list_index + 1 == len(list1):
-                    return True
-                list_index += 1
-            elif compare < 0:
-                if container_index + 1 == len(container):
-                    return False
-                container_index += 1
-            else:
-                return False
-
-    def compare_by_filename(self, part1: ClonePart, part2: ClonePart):
-        filename_1 = part1.get__filename()
-        filename_2 = part2.get__filename()
-
-        if filename_1 == filename_2:
-            return 0
-        elif filename_1 < filename_2:
-            return -1
-        else:
-            return 1
-
-    def compare_by_filename_and_unit(self, part1: ClonePart, part2: ClonePart):
-        compare = self.compare_by_filename(part1, part2)
-
-        if compare == 0:
-            if part1.get__unit_start() <= part2.get__unit_start():
-                if part2.get__unit_start() + self.l2 <= part1.get__unit_start() + self.l1:
-                    return 0
-                else:
-                    return -1
-            else:
-                return 1
-        else:
-            return compare
+        self.filter.add_in_filter(clone_group)
