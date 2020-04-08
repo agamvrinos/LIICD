6cecbb4b14008acfb8f280f537aeb84f3dfcdacf
agamvrinos <agg_gam@hotmail.com>
2020-04-08 17:31:40 +0200

    Add CodebaseReader

---
+++ LSH-based/detector/CodebaseReader.py
@@ -0,0 +1,80 @@
+import os
+
+
+class CodebaseReader:
+    """
+    Recursively reads all the files under the given path and stores
+    the files themselves as well as the lines corresponding to each file.
+
+    Args:
+        files (str[]): The paths of the files under the root directory
+        lines_per_file (dict): A dictionary with files pointing to the
+        respective lines per file.
+
+    Attributes:
+        path (POSIX Path): The path pointing to the root of the codebase
+        to be read.
+    """
+
+    files = []
+    lines_per_file = {}
+    total_lines = 0
+    SKIP_DIRS = [
+        'node_modules', 'assets', 'build', 'classes', 'gradle', 'licenses'
+    ]
+    SKIP_FILES = [
+        '.tar', '.ico', '.png', '.jpg', '.jpeg', '.txt', '.md', '.bat', '.sh', '.jks',
+        '.prpt', '.pyc', '.bmp', '.ini', '.db', '.gif', '.plist', '.ver', '.ogv',
+        '.swf', '.xpi', '.icns', '.ogg', ".eot", ".ttf", ".woff"
+    ]
+
+    def __init__(self, path):
+        self.path = str(path)
+        for root, directories, filenames in os.walk(self.path):
+            filenames2 = []
+            for f in filenames:
+                split_path = f.split(os.sep)
+                is_without_extension = len(split_path[len(split_path) - 1].split(".")) == 1
+                if not (f[0] == '.' or f.endswith(tuple(self.SKIP_FILES)) or is_without_extension):
+                    filenames2.append(f)
+                # else:
+                #     print("Skipping file \"" + f + "\"")
+
+            filenames = filenames2
+
+            directories[:] = [d for d in directories if not (d[0] == '.' or d in self.SKIP_DIRS)]
+
+            for filename in filenames:
+                join = os.path.join(root, filename)
+                self.files.append(join)
+                lines = CodebaseReader.get_lines_for_file(join)
+                self.lines_per_file[join] = lines
+                self.total_lines = self.total_lines + len(lines)
+
+    def get__path(self):
+        return self.path
+
+    def get_files(self):
+        return self.files
+
+    def get_lines_per_file(self):
+        return self.lines_per_file
+
+    def get__initial_codebase_lines(self):
+        return self.total_lines
+
+    @staticmethod
+    def get_lines_for_file(path):
+        try:
+            with open(path, encoding='utf-8') as fp:
+                lines = []
+                for line in fp:
+                    line = line.strip()
+                    line = " ".join(line.split())
+                    if line:
+                        lines.append(line)
+            fp.close()
+            return lines
+        except:
+            print("Error for file: ", path)
+            raise
--- LSH-based/detector/main.py
+++ LSH-based/detector/main.py
@@ -1,1 +1,20 @@
-print("Hello LSH-based approach :)")
+from pathlib import Path
+from datasketch import MinHash, MinHashLSH
+from timeit import default_timer as timer
+from detector.CodebaseReader import CodebaseReader
+
+project_path = input("Provide the project path: ")
+if not project_path:
+    project_path = Path.home() / 'Desktop/data/test_project'
+    print("Empty path provided. Setting default project path \"" + str(project_path) + "\"")
+
+# start the timer
+# start = timer()
+
+codebase = CodebaseReader(project_path)
+lines_per_files = codebase.get_lines_per_file()
+
+for file in lines_per_files:
+    print(file)
+    for line in lines_per_files[file]:
+        print(line)
