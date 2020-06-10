7284083cd6cee1052c4fa5cf9f15271c5e73150d
agamvrinos <agg_gam@hotmail.com>
2020-06-10 08:09:45 +0200

    Original: Add and pretty-print measurements

--- Original/detector/main.py
+++ Original/detector/main.py
@@ -21,7 +21,7 @@ def run(codebase_path, updates_file_path):
                    stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
     # start the timer
-    start = timer()
+    index_cr_start = timer()
 
     codebase = CodebaseReader(codebase_path)
     lines_per_files = codebase.get_lines_per_file()
@@ -32,30 +32,32 @@ def run(codebase_path, updates_file_path):
         index_entries = clone_index.calculate_index_entries_for_file(file, lines_per_files[file])
         clone_index.add_index_entries(index_entries)
 
-    end = timer()
-
-    print("============================================================")
-    print("Total LOCs: ", str(codebase.get__initial_codebase_lines()))
-    print("Index creation time: " + str(round(end-start, 5)) + " seconds")
-    print("============================================================")
+    index_cr_end = timer()
+    index_cr_diff = round(index_cr_end - index_cr_start, 5)
 
     detector = CloneDetector(clone_index)
+
+    incremental_step_time = 0
+    commits_processed = 0   # we use this instead of len(data['commits']) bcs there might me commits that only affect
+                            # excluded (e.g. test) files and in that case the specific commit does not get processed
 
     try:
         with open(updates_file_path) as f:
             data = json.load(f)
+            commits = data['commits']
 
-            for commit in data['commits']:
+            for commit in commits:
                 creates_lst = []
                 updates_lst = []
                 deletes_lst = []
 
-                print('============================================================')
-                print('Running Analysis for codebase @commit: ', commit['id'])
+                print('========> Running Analysis for codebase @commit: ', commit['id'], "<========")
 
                 # checkout to the current commit
-                subprocess.run(['git', '-C', str(codebase_path),
-                                'checkout', commit['id']], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+                subprocess.run(['git', '-C', str(codebase_path), 'checkout', commit['id']],
+                               stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+
+                is_processed = False
 
                 for change in commit['changes']:
                     change_type = change['type']
@@ -77,6 +79,8 @@ def run(codebase_path, updates_file_path):
                     if file_path.suffix in config.SKIP_FILES:
                         continue
 
+                    is_processed = True  # if I get here then there is at least 1 change in that commit that is processed
+
                     file_path = codebase_path / file_path
 
                     print('-> Parsing change [', change_type, '] for file [', file_path, ']')
@@ -88,19 +92,36 @@ def run(codebase_path, updates_file_path):
                     elif change_type == 'D':
                         deletes_lst.append(str(file_path))
 
-                changes_handler = ChangesHandler(detector, deletes_lst, updates_lst, creates_lst)
-                # start incremental step timer
-                start = timer()
-                # handle commit changes
-                changes_handler.handle_changes()
-                # end incremental step timer
-                end = timer()
-                print("Detection/Index update time: " + str(round(end - start, 5)) + " seconds")
-                print('=======================================================')
+                if is_processed:
+                    changes_handler = ChangesHandler(detector, deletes_lst, updates_lst, creates_lst)
+                    # start incremental step timer
+                    start = timer()
+                    # handle commit changes
+                    changes_handler.handle_changes()
+                    # end incremental step timer
+                    end = timer()
+                    time_diff = round(end - start, 5)
+                    print("Detection/Index update time: " + str(time_diff) + " seconds")
+
+                    commits_processed += 1
+                    incremental_step_time += time_diff
+                else:
+                    print("Commit " + commit['id'] + " was skipped because all files were excluded")
 
                 # checkout back to HEAD
                 subprocess.run(['git', '-C', str(codebase_path), 'checkout', '-'],
                                stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+
+            print("============================================================")
+            print("Total LOCs: ", codebase.get__initial_codebase_lines())
+            print("Total Index creation time: ", index_cr_diff, " seconds")
+            print("Total commits: ", len(commits))
+            print("Total commits processed: ", commits_processed)
+            if commits_processed > 0:
+                print("Average Incremental Step Time: ", incremental_step_time / float(commits_processed))
+            else:
+                print("0 commits out of ", len(commits), " were processed. Something went terribly wrong!")
+            print("============================================================")
 
         f.close()
     except IOError:
